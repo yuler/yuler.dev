0bb8dec0917b00f45f15b189879b0cb0f45a4a80
yuler <is.yuler@gmail.com>
2026-04-03 19:50:05 +0800

    📦 Add dprint markdown formatter for ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,8 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   astro: true,
+  formatters: {
+    /** Markdown: dprint via eslint-plugin-format (`format/dprint`, language `markdown`) */
+    markdown: 'dprint',
+  },
 })
